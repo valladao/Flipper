f090b5e77261807dc8541ad3fb4ae7b6d712fb99
Manoel Valladao <valladao@gmail.com>
2021-06-12 11:05:32 -0300

    Post a message

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express")
-const app = express()
-
 const path = require("path")
-
 const redis = require("redis")
-const client = redis.createClient()
-
 const bcrypt = require("bcrypt")
 const session = require("express-session")
+
+const app = express()
+const client = redis.createClient()
+
 const RedisStore = require("connect-redis")(session)
 
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({ extended: true }))
-
 app.use(
   session({
     store: new RedisStore({ client: client }),
@@ -37,13 +35,15 @@ app.get("/", (req, res) => {
   }
 })
 
-app.post("/", (req, res) => {
-  const saveSessionAndRenderDashboard = (userid) => {
-    req.session.userid = userid
-    req.session.save()
-    res.render("dashboard")
+app.get("/post", (req, res) => {
+  if (req.session.userid) {
+    res.render("post")
+  } else {
+    res.render("login")
   }
+})
 
+app.post("/", (req, res) => {
   const { username, password } = req.body
 
   if (!username || !password) {
@@ -53,34 +53,68 @@ app.post("/", (req, res) => {
     return
   }
 
-  console.log(req.body, username, password)
+  const saveSessionAndRenderDashboard = (userid) => {
+    req.session.userid = userid
+    req.session.save()
+    res.render("dashboard")
+  }
+
+  const handleSignup = (username, password) => {
+    client.incr("userid", async (err, userid) => {
+      client.hset("users", username, userid)
+      const saltRounds = 10
+      const hash = await bcrypt.hash(password, saltRounds)
+      client.hset(`user:${userid}`, "hash", hash, "username", username)
+      saveSessionAndRenderDashboard(userid)
+    })
+  }
+
+  const handleLogin = (userid, password) => {
+    client.hget(`user:${userid}`, "hash", async (err, hash) => {
+      const result = await bcrypt.compare(password, hash)
+      if (result) {
+        //password ok
+        saveSessionAndRenderDashboard(userid)
+      } else {
+        //wrong password
+        res.render("error", {
+          message: "Incorrect password",
+        })
+        return
+      }
+    })
+  }
 
   client.hget("users", username, (err, userid) => {
     if (!userid) {
-      //user does not exist, signup procedure
-      client.incr("userid", async (err, userid) => {
-        client.hset("users", username, userid)
-        const saltRounds = 10
-        const hash = await bcrypt.hash(password, saltRounds)
-        client.hset(`user:${userid}`, "hash", hash, "username", username)
-        saveSessionAndRenderDashboard(userid)
-      })
+      //signup procedure
+      handleSignup(username, password)
     } else {
       //user exists, login procedure
-      client.hget(`user:${userid}`, "hash", async (err, hash) => {
-        const result = await bcrypt.compare(password, hash)
-        if (result) {
-          //password ok
-          saveSessionAndRenderDashboard(userid)
-        } else {
-          //wrong password
-          res.render("error", {
-            message: "Incorrect password",
-          })
-          return
-        }
-      })
+      handleLogin(userid, password)
     }
+  })
+})
+
+app.post("/post", (req, res) => {
+  if (!req.session.userid) {
+    res.render("login")
+    return
+  }
+
+  const { message } = req.body
+
+  client.incr("postid", async (err, postid) => {
+    client.hmset(
+      `post:${postid}`,
+      "userid",
+      req.session.userid,
+      "message",
+      message,
+      "timestamp",
+      Date.now()
+    )
+    res.render("dashboard")
   })
 })
 
